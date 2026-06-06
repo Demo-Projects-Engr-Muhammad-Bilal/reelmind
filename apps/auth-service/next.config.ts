c29085b4f3306498554635f2205766f962c09fc9
Muhammad Bilal <muhammadbilal41266@gmail.com>
2026-06-06 07:51:20 +0500

    fix: add tailwindcss postcss to auth-service

--- apps/auth-service/next.config.ts
+++ apps/auth-service/next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   experimental: {
     serverActions: {
       allowedOrigins: [
         "localhost:3000",
-        "*.tunnels.ms", // For VS Code Port Forwarding
+        "*.tunnels.ms",
         "*.ngrok.io",
         "*.ngrok-free.app"
       ],
